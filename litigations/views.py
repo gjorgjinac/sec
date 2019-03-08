d89bfc94343fd789f4e69938d35137c5f10177e5
Gjorgjina Cenikj <gjorgjinac@gmail.com>
2019-03-08 10:41:25 +0100

    enabled filtering with multiple parameters

--- litigations/views.py
+++ litigations/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, generics
 from litigations.models import Litigation, Reference, Title
 from litigations.serializers import LitigationSerializer, ReferenceSerializer, TitleSerializer
-
+import datetime
 
 class LitigationViewSet(viewsets.ModelViewSet):
     queryset = Litigation.objects.all()
@@ -12,18 +12,25 @@ class LitigationViewSet(viewsets.ModelViewSet):
         orgs = self.request.GET.get('organizations')
         people = self.request.GET.get('people')
         respondents = self.request.GET.get('respondents')
-
+        date1=self.request.GET.get('from')
+        date2=self.request.GET.get('to')
+        allLitigations=Litigation.objects.all();
         if relno is not None:
-            return Litigation.objects.filter(release_no=relno)
+            allLitigations=allLitigations.filter(release_no=relno)
         if year is not None:
-            return Litigation.objects.filter(date__year=year)
+            allLitigations = allLitigations.filter(date__year=year)
         if orgs is not None:
-            return Litigation.objects.filter(organizations__contains=orgs)
+            allLitigations = allLitigations.filter(organizations__contains=orgs)
         if people is not None:
-            return Litigation.objects.filter(people__contains=people)
+            allLitigations = allLitigations.filter(people__contains=people)
         if respondents is not None:
-            return Litigation.objects.filter(respondents__contains=respondents)
-        return Litigation.objects.all()
+            allLitigations = allLitigations.filter(respondents__contains=respondents)
+        if date1 is not None:
+            allLitigations = allLitigations.filter(date__gte = datetime.date(int(date1.split("-")[0]),int(date1.split("-")[1]),int(date1.split("-")[2])))
+        if date2 is not None:
+            allLitigations = allLitigations.filter(date__lte = datetime.date(int(date2.split("-")[0]),int(date2.split("-")[1]),int(date2.split("-")[2])))
+
+        return allLitigations
 
 
 class ReferenceViewSet(viewsets.ModelViewSet):
